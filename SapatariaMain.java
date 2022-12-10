d247adaae140cd64be04e54009a131eb1efc8550
karen <karenamanda56@gmail.com>
2022-12-10 13:39:20 -0300

    Herança e Polimorfismo

---
+++ SapatariaMain.java
@@ -0,0 +1,24 @@
+import java.math.BigDecimal;
+
+public class SapatariaMain {
+    public static void main(String[] args) {
+		
+		Sapataria produtoSapataria = new SapatariaTenis(300,100,30,BigDecimal.valueOf(350L));
+		System.out.println("\nTenis");
+		produtoSapataria.preparar();
+		produtoSapataria.execucao();
+		produtoSapataria.vender();
+		
+		produtoSapataria = new SapatariaSapato(500,200,120,BigDecimal.valueOf(350L));
+		System.out.println("\nSapato");
+		produtoSapataria.preparar();
+		produtoSapataria.execucao();
+		produtoSapataria.vender();
+				
+		produtoSapataria = new SapatariaChinelo();
+        System.out.println("\nChinelo");
+        produtoSapataria.preparar();
+        produtoSapataria.execucao();
+        produtoSapataria.vender();
+	}
+}
